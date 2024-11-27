8fc347bcd9cf0519107052fcf8ed9991cd2509de
ajonmarc <119939476+ajonmarc@users.noreply.github.com>
2024-11-27 18:40:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+// Add the import below
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        // Add this vue plugin configuration here
         vue({
             template: {
                 transformAssetUrls: {
@@ -15,30 +17,6 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
-        }),
+        })
     ],
-    build: {
-        cssCodeSplit: true, // Enable CSS code splitting
-        chunkSizeWarningLimit: 1500, // Suppress chunk size warnings
-        minify: 'esbuild', // Use esbuild for faster builds
-        sourcemap: false, // Disable source maps for production builds
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('vue')) {
-                            return 'vue'; // Separate Vue into its own chunk
-                        }
-                        if (id.includes('vue-router')) {
-                            return 'vue-router'; // Separate vue-router
-                        }
-                        if (id.includes('axios')) {
-                            return 'axios'; // Separate axios
-                        }
-                        return 'vendor'; // All other vendor libraries
-                    }
-                },
-            },
-        },
-    },
 });
