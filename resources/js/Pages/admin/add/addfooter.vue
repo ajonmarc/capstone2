e9928cf06541b945dc3767dd0ff9de7806967eee
Aljonmarc <acortel@ssct.edu.ph>
2024-11-27 19:58:15 +0800

    unhide

--- resources/js/Pages/admin/add/addfooter.vue
+++ resources/js/Pages/admin/add/addfooter.vue
@@ -160,7 +160,7 @@ const closeModal = () => {
         <!--form end-->
     </el-dialog>
 
-    <button style="display: none;"
+    <button 
         type="button" 
         @click="openAddF"
         class="ml-2 mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
