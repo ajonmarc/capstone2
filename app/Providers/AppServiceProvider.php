c5d4a39203f20f6ead6ae53037c56b2b4ed1bf07
Aljonmarc <acortel@ssct.edu.ph>
2024-11-27 12:18:14 +0800

    serviceP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +19,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(UrlGenerator $url): void
     {
+        // Enforce HTTPS if the environment variable ENFORCE_SSL is true
+        if (env('ENFORCE_SSL', false)) {
+            $url->forceScheme('https');
+        }
+
+        // Set the default string length for database migrations
         Schema::defaultStringLength(191);
     }
 }
