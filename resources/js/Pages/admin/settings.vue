f678b2ab16aa551bf40f17b3e25d56e220a7f5be
Aljonmarc <acortel@ssct.edu.ph>
2024-11-28 16:28:24 +0800

    editedHeader and Users

--- resources/js/Pages/admin/settings.vue
+++ resources/js/Pages/admin/settings.vue
@@ -271,7 +271,7 @@ const closeModal = () => {
 
             <button
                 type="submit"
-                class=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
             >
                 Submit
             </button>
--- resources/js/Pages/admin/users.vue
+++ resources/js/Pages/admin/users.vue
@@ -293,7 +293,7 @@ const closeViewModal = () => {
                 <select
                     id="role"
                     v-model="form.role"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 >
                     <option value="">Choose role</option>
                     <option value="doctor">DOCTOR</option>
